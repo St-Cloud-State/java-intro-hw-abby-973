858c47b91ec98a3b3595c4c3004603269393d2bb
Abby Sininger <143823566+abby-973@users.noreply.github.com>
2024-09-07 01:35:58 +0000

    fixed wrong code

--- Hw1Q3/PersonList.java
+++ Hw1Q3/PersonList.java
@@ -3,68 +3,45 @@ import java.io.FileNotFoundException;
 import java.util.*;
 //Inports the linked list java class
 import java.util.LinkedList;
+import java.util.stream.Stream;
 
-public class Person 
-{
-
-  //Create all the string fields
-  private String firstName;
-  private String lastName;
-  private int Id;
-
-  //Person Constructor
-  Person(String fname, String lname, int id)
-  {
-    firstName = fname;
-    lastName = lname;
-    Id = id;
-  }
-
-//Declaring the accessor functions
-  public String getFirstName()
-  {
-    return firstName;
-  }
-  public String getLastName()
-  {
-    return lastName;
-  }
-  public int getId()
-  {
-    return Id;
-  }
-
-  //toString method
-  public String toString(){
-    return "The person's First Name is: " + firstName + ", Last Name is: " + lastName + ", and Id is:" + Id ;
-  }
-}
-
-class MyMain 
-{
-    //Store function to store each person in a linked lsit
-    public static void store (Scanner scanner, LinkedList<Person> list)
+//Person Linked List class
+public class PersonList {
+    
+    private LinkedList<Person> list;
+    
+    //Constructor
+    PersonList()
     {
-        // Scans each line as long as there is a person
+        list = new LinkedList<Person>();
+    }
+
+    //Accesor function
+    public LinkedList<Person> getList()
+    {
+        return list;
+    }
+
+    //Store function
+    public void store (Scanner scanner)
+    {
       while (scanner.hasNextLine()) {
           String[] parts = scanner.nextLine().split(",");
           list.add(new Person(parts[0], parts[1], Integer.parseInt(parts[2])));
       }
     }
 
-    //Display function to print out the linked list
-    public static void display (PrintStream outputStream, LinkedList<Person> list)
+    //Display function
+    public void display (PrintStream outputStream)
     {
-        //Print out for each item in the list
         for (int i = 0; i < list.size(); i++) { 
             outputStream.println(list.get(i).toString()); 
         }
     }
 
-    //Find function to find a person by the id
-    public static int find (int sid, LinkedList<Person> list) 
+    //Find Function
+    public int find (int sid) 
     {
-        //Searches for user
         for (int i=0; i < list.size(); i++)
         {
             if (list.get(i).getId() == sid) 
@@ -72,20 +49,18 @@ class MyMain
                 return i;
             }
         }
-        //If user couldn't be found return -1
         return -1;
     }
+}
 
-
-    public static void main(String[] args) throws FileNotFoundException 
-    {
-
+class MyMainQ3 {
+    public static void main(String[] args) throws FileNotFoundException {
       String fileName1, outFileName;
       String findAnother;
       File dataFile1, outFile;
       boolean findNext = true;
       int findID;
-      LinkedList<Person> personList = new LinkedList<>();
+      PersonList personList = new PersonList();
       Scanner keyScanner = new Scanner(System.in);
       //Ask user for input file to be scanned
       System.out.println("Please eneter the name of your input file: ");
@@ -97,19 +72,19 @@ class MyMain
       outFile = new File(outFileName);
       Scanner fileScanner1 = new Scanner(dataFile1);
       PrintStream printer = new PrintStream(outFile);
-      //Store function until print function 
-      store(fileScanner1, personList);
+      personList.store(fileScanner1);
       System.out.println("Displaying every person in the list:");
-      display(printer, personList);
+      personList.display(printer);
+
       while (findNext)
       {
         System.out.println("Please input id to find: ");
         findID = Integer.parseInt(keyScanner.nextLine());
-        int index = find(findID, personList);
+        int index = personList.find(findID);
         if (index != -1) {
-            System.out.println("Person with Id " + personList.get(index).getId() + " was found: "+ personList.get(index).toString());
+            System.out.println("Person with Id " + personList.getList().get(index).getId() + " was found: "+ personList.getList().get(index).toString());
         } else {
-            System.out.println("Person with Id " + personList.get(index).getId() + " not found.");
+            System.out.println("Person with Id " + personList.getList().get(index).getId() + " not found.");
         }
         System.out.println("would you like to continue (y/n): ");
         findAnother = keyScanner.nextLine();
